564d067785845962bb650586fb3098d0e7cc22e9
Hoby Ratefy <hoby.ratefy@protonmail.com>
2023-04-13 17:13:18 +0900

    Change game logic and add game loop

--- app.js
+++ app.js
@@ -1,50 +1,69 @@
-// Let player choose move -> getPlayerChoice()
-let playerSelection = prompt("What's your move (rock, scissors or paper)? ").toLowerCase();
-
 // Make computer choose move -> getComputerChoice()
 function getComputerChoice() {
   const moves = ['rock', 'paper', 'scissors'];
   return moves[Math.floor(Math.random() * 3)];
 }
 
-const computerSelection = getComputerChoice();
-
 // Making a game function
 function playRound(playerSelection, computerSelection) {
-  let isPlayerWinner = false;
+  let isPlayerWinner = 0;
   // If same moves : draw
   if (playerSelection === computerSelection) {
-    return "DRAW! No winner, no loser!"
+    isPlayerWinner = -1;
   }
   // If player chooses Rock, compare with other cases from computer
-  if (playerSelection === 'rock') { 
+  else if (playerSelection === 'rock') { 
     if (computerSelection === 'scissors') {
-      isPlayerWinner = true;
+      isPlayerWinner = 1;
     } else if (computerSelection === 'paper') {
-      isPlayerWinner = false;
+      isPlayerWinner = 0;
     }
   }
   // If player chooses Paper, compare with other cases from computer
   else if (playerSelection === 'paper') { 
     if (computerSelection === 'rock') {
-      isPlayerWinner = true;
+      isPlayerWinner = 1;
     } else if (computerSelection === 'scissors') {
-      isPlayerWinner = false;
+      isPlayerWinner = 0;
     }
   }
   // If player chooses Scissors, compare with other cases from computer
   else if (playerSelection === 'scissors') { 
     if (computerSelection === 'paper') {
-      isPlayerWinner = true;
+      isPlayerWinner = 1;
     } else if (computerSelection === 'rock') {
-      isPlayerWinner = false;
+      isPlayerWinner = 0;
     }
   }
 
-  if (isPlayerWinner) {
-    return `You win! ${playerSelection} beats ${computerSelection}`;
+  if (isPlayerWinner === 1) {
+    console.log(`You win! ${playerSelection} beats ${computerSelection}`);
+  } else if (isPlayerWinner === 0) {
+    console.log(`You lose! ${computerSelection} beats ${playerSelection}`);
+  } else {
+    console.log("DRAW! No winner, no loser!");
   }
-  return `You lose! ${computerSelection} beats ${playerSelection}`;
+
+  return isPlayerWinner;
 }
 
-console.log(playRound(playerSelection, computerSelection));
+function game() {
+  let playerScore = 0;
+  let computerScore = 0;
+  for (let i = 0; i < 5; i++) {
+//    let playerSelection = prompt("What's your move (rock, scissors or paper)? ").toLowerCase();
+    const playerSelection = getComputerChoice();
+    const computerSelection = getComputerChoice();
+    result = playRound(playerSelection, computerSelection);
+    if(result === 1) {
+      playerScore++;
+    } else if (result === 0){
+      computerScore++;
+    } else {
+      // do not update scores
+    }
+    console.log(`Player ${playerScore} - ${computerScore} Computer`);
+  }
+}
+
+game();
