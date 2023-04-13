7bf48454ac0fc515a41882fbd53561556c2906eb
Hoby Ratefy <hoby.ratefy@protonmail.com>
2023-04-13 16:31:08 +0900

    Add game logic and user input

--- app.js
+++ app.js
@@ -1,14 +1,44 @@
 // Let player choose move -> getPlayerChoice()
+let playerSelection = prompt("What's your move (rock, scissors or paper)? ")
+playerSelection = playerSelection.toLowerCase();
+
 // Make computer choose move -> getComputerChoice()
 function getComputerChoice() {
   const moves = ['rock', 'paper', 'scissors'];
   return moves[Math.floor(Math.random() * 3)];
 }
+
+const computerSelection = getComputerChoice();
+
 // Using comparisons, decide who wins or when is draw.
-// If Rock vs. Paper : Paper wins
-// If Rock vs. Scissors : Rock wins
-// If Paper vs. Scissors : Scissors wins
-//
-for (let i = 0; i < 5; i++) {
-  console.log(getComputerChoice()); 
+// If same moves : draw
+if (playerSelection === computerSelection) {
+  console.log('DRAW!');
 }
+
+// If player chooses Rock, compare with other cases from computer
+if (playerSelection === 'rock') { 
+  if (computerSelection === 'scissors') {
+    console.log('Player wins!');
+  } else if (computerSelection === 'paper') {
+    console.log('Computer wins!');
+  }
+}
+// If player chooses Paper, compare with other cases from computer
+else if (playerSelection === 'paper') { 
+  if (computerSelection === 'rock') {
+    console.log('Player wins!');
+  } else if (computerSelection === 'scissors') {
+    console.log('Computer wins!');
+  }
+}
+// If player chooses Scissors, compare with other cases from computer
+else if (playerSelection === 'scissors') { 
+  if (computerSelection === 'paper') {
+    console.log('Player wins!');
+  } else if (computerSelection === 'rock') {
+    console.log('Computer wins!');
+  }
+}
+
+console.log(`Player: ${playerSelection} - Computer: ${computerSelection}`);
